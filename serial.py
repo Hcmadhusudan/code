8c5a0f3997e9afa70abd44d7ba6ac096ca5acac6
Natanael <natanael_gomes@msn.com>
2023-01-10 14:52:39 +0100

    Receiving data
    Included the class QMainWindow to keep the app running.

--- serial.py
+++ serial.py
@@ -3,35 +3,53 @@ from PyQt5.QtCore import (
     pyqtSlot, QByteArray, QObject, pyqtSignal, QRunnable, Qt, QTimer, QTime, QSettings, QSize, QThread, QEventLoop, QIODevice,
 )
 import time
+from PyQt5.QtWidgets import (
+    QApplication,
+    QMainWindow,
+    QWidget,
+)
+import sys
+import re
+
+class MainWindow(QMainWindow):
+    def __init__(self, *args, **kwargs):
+        super(MainWindow, self).__init__(*args, **kwargs)
+        self.serial = QtSerialPort.QSerialPort('COM3')
+        self.serial.setBaudRate(115200)
+        self.serial.readyRead.connect(self.receive)
+        a = self.serial.open(QIODevice.ReadWrite)
+        print(a)
+
+    @pyqtSlot()
+    def receive(self):
+        try:
+            while self.serial.canReadLine():
+                text = self.serial.readLine().data().decode()
+                text = text.rstrip('\r\n')
+                result = re.findall(r"[-+]?(?:\d*\.\d+|\d+)", text)
+
+                if "Received" in text:
+                    flow_index = int(result[1])
+                    flow_voltage = float(result[2])
+                    print(flow_voltage)
+                    # print("Received Serial:", value)
+                    # self.new_data.emit(value)
+                elif "Temperature" in text:
+                    pass
+                    temp_index = int(result[1])
+                    temp_voltage = float(result[2])
+                else:
+                    print(text)
+
+        except Exception as ex:
+            print(ex)
 
 
-def receive():
-    while serial.canReadLine():
-        text = serial.readLine().data().decode()
-        text = text.rstrip('\r\n')
-        result = re.findall(r"[-+]?(?:\d*\.\d+|\d+)", text)
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
 
-        # print(text.split())
-        # print(result)
-        if "Received" in text:
-            flow_index = int(result[1])
-            flow_voltage = float(result[2])
-            # print("Received Serial:", value)
-            # self.new_data.emit(value)
-        elif "Temperature" in text:
-            temp_index = int(result[1])
-            temp_voltage = float(result[2])
-
-        else:
-            print(text)
-        #
-        if flow_index == self.temp_index:
-            add_data_point(flow_voltage, None, [temp_voltage, 0.0, 0.0, 0.0])
-serial = QtSerialPort.QSerialPort('COM7')
-serial.setBaudRate(115200)
-serial.readyRead.connect(receive)
-a=serial.open(QIODevice.ReadWrite)
-print(a)
-
+    w = MainWindow()
+    # w.show()
+    sys.exit(app.exec())
 #serial = QtSerialPort.QSerialPort('COM7', QtSerialPort.QSerialPort.Baud115200, receive)
 #serial
